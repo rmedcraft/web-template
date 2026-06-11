0061158f3e7742b4d0b405e2d4b6ea66a83ea504
rmedcraft <rowanmedcraft04@gmail.com>
2026-06-11 18:56:03 -0400

    tailwind v4 setup

---
+++ README.md
@@ -0,0 +1,9 @@
+This is a template for web projects using
+- Typescript (~6.0.2)
+- React (^19.2.6)
+- TailwindCSS (^4.3.0)
+- Vite (^8.0.12)
+
+This was set up on WSL Ubuntu 24.04.3
+
+### Setup
---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+    return (
+        <>
+            <div className='text-7xl bg-amber-600 p-10'>
+                <h1>HELP!!</h1>
+            </div>
+        </>
+    )
+}
+
+export default App
