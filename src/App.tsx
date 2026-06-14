6c7f261cabccbc8d5db3a2163c0ad36d4e0e591e
rmedcraft <rowanmedcraft04@gmail.com>
2026-06-14 14:39:47 -0400

    better intro stuling

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,10 @@ import { NavLink } from "react-router"
 
 function App() {
     return (
-        <>
-            <div className=' bg-gray-300 dark:bg-gray-700 dark:text-white p-10'>
+        <div className="bg-linear-to-br from-gray-900 to-green-600 h-screen w-screen">
+            <div className=' text-white p-10 container h-fit m-auto'>
                 <h1 className="text-7xl pb-5">Welcome to my template!</h1>
+                <h1 className="text-muted pb-5">This is a template for starting a project with Typescript, React, Vite, React Router, TailwindCSS, Shadcn. Delete the code in <span className="monospace">main.tsx</span> and start making your project</h1>
 
                 {/* https://reactrouter.com/start/declarative/navigating 
                     <Link> can't generate an active state, <NavLink> can
@@ -14,7 +15,7 @@ function App() {
                     <Button variant={"default"} className="p-4 active:bg-blue-500">About</Button>
                 </NavLink>
             </div>
-        </>
+        </div>
     )
 }
 
--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -3,15 +3,28 @@ import { NavLink } from "react-router";
 
 export default function About() {
     return (
-        <>
-            <div>
-                <div className=' bg-gray-300 dark:bg-gray-700 dark:text-white p-10'>
-                    <h1 className="text-7xl pb-5">Welcome to my about page!</h1>
-                    <NavLink to="/">
-                        <Button variant={"default"} className="p-4 active:bg-blue-500">Home</Button>
-                    </NavLink>
-                </div>
+        <div className="bg-linear-to-br from-gray-900 to-green-600 h-screen w-screen">
+            <div className=' text-white p-10 container h-fit m-auto'>
+                <h1 className="text-7xl pb-5">Welcome to the about page</h1>
+                <h1 className="text-muted pb-5">This page is only for showing that react router exists</h1>
+
+                {/* https://reactrouter.com/start/declarative/navigating 
+                    <Link> can't generate an active state, <NavLink> can
+                */}
+                <NavLink to="/">
+                    <Button variant={"default"} className="p-4 active:bg-blue-500">Home</Button>
+                </NavLink>
             </div>
-        </>
+        </div>
+        // <>
+        //     <div>
+        //         <div className=' bg-gray-300 dark:bg-gray-700 dark:text-white p-10'>
+        //             <h1 className="text-7xl pb-5">Welcome to my about page!</h1>
+        //             <NavLink to="/">
+        //                 <Button variant={"default"} className="p-4 active:bg-blue-500">Home</Button>
+        //             </NavLink>
+        //         </div>
+        //     </div>
+        // </>
     )
 }
